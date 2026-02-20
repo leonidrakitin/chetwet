8a6e0827e30003d104b10f1ed5ce8d5e4a774bbe
Ракитин Леонид Павлович <lrakitin@alfabank.ru>
2026-02-21 00:27:42 +0300

    feat(captain): test 3

--- enterprise/app/services/captain/llm/translate_query_service.rb
+++ enterprise/app/services/captain/llm/translate_query_service.rb
@@ -1,5 +1,5 @@
 class Captain::Llm::TranslateQueryService < Captain::BaseTaskService
-  MODEL = 'aroxima/gte-qwen2-1.5b-instruct:q4_k_m'.freeze
+  MODEL = 'deepseek-chat'.freeze
 
   pattr_initialize [:account!]
 
