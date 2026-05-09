22f104729db5ed9f6c926997e2fa21fc49a8244f
Ракитин Леонид Павлович <lrakitin@alfabank.ru>
2026-05-09 10:19:00 +0300

    fix(*): fixed Dockerfile 3

--- enterprise/app/controllers/enterprise/api/v1/accounts_controller.rb
+++ enterprise/app/controllers/enterprise/api/v1/accounts_controller.rb
@@ -16,7 +16,7 @@ class Enterprise::Api::V1::AccountsController < Api::BaseController
     limits = if default_plan?(@account)
                {
                  'conversation' => {
-                   'allowed' => 400,
+                   'allowed' => 500,
                    'consumed' => conversations_this_month(@account)
                  },
                  'non_web_inboxes' => {
